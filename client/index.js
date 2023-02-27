dedbfaa0eb4f1a4a8181481da03e2f2377628a2f
gylman <gylmanbm@gmail.com>
2023-02-27 23:56:09 +0900

    add input box

--- client/index.js
+++ client/index.js
@@ -19,14 +19,10 @@ const messages = [
   { text: "Dankes", direction: "sent" },
 ];
 
-const messagesBox = `<div class="messages-box"></div>`;
-
 const messageRow = (message, direction) =>
   direction === "sent"
     ? `<div class="message-row"><p class="message sent">${message}</p></div>`
     : `<div class="message-row received"><p class="message">${message}</p></div>`;
-
-afterBegin(select("body"), messagesBox);
 
 messages.forEach((message) => {
   beforeEnd(
@@ -34,10 +30,3 @@ messages.forEach((message) => {
     messageRow(message.text, message.direction)
   );
 });
-
-document.querySelector(".form-send").addEventListener("submit", sendEmail);
-
-function sendEmail(event) {
-  event.preventDefault();
-  console.log("check");
-}
