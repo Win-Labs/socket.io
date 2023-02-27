1a4d9b56fdf9451c91c2ee9e9decef50ea749604
gylman <gylmanbm@gmail.com>
2023-02-28 02:07:57 +0900

    add reply button and reifne styling

--- client/index.js
+++ client/index.js
@@ -16,13 +16,47 @@ const messages = [
   { text: "You?", direction: "received" },
   { text: "I am okay", direction: "sent" },
   { text: "Thankos", direction: "received" },
+  {
+    text: "The Battle of Badr was the first large-scale confrontation between Muslims and the Quraysh.",
+    direction: "sent",
+  },
+  {
+    text: "More than a thousand men engaged in the battle, which lasted hours and resulted in a decisive Muslim victory. ",
+    direction: "received",
+  },
   { text: "Dankes", direction: "sent" },
+  {
+    text: "More than a thousand men engaged in the battle, which lasted hours and resulted in a decisive Muslim victory. ",
+    direction: "sent",
+  },
+  { text: "Dankes", direction: "received" },
+  {
+    text: "More than a thousand men engaged in the battle, which lasted hours and resulted in a decisive Muslim victory. ",
+    direction: "received",
+  },
+  { text: "Dankes", direction: "sent" },
+  {
+    text: "More than a thousand men engaged in the battle, which lasted hours and resulted in a decisive Muslim victory. ",
+    direction: "sent",
+  },
+  {
+    text: "More than a thousand men engaged in the battle, which lasted hours and resulted in a decisive Muslim victory. ",
+    direction: "received",
+  },
+  {
+    text: "More than a thousand men engaged in the battle, which lasted hours and resulted in a decisive Muslim victory. ",
+    direction: "sent",
+  },
+  {
+    text: "More than a thousand men engaged in the battle, which lasted hours and resulted in a decisive Muslim victory. ",
+    direction: "received",
+  },
 ];
 
 const messageRow = (message, direction) =>
   direction === "sent"
-    ? `<div class="message-row"><p class="message sent">${message}</p></div>`
-    : `<div class="message-row received"><p class="message">${message}</p></div>`;
+    ? `<div class="message-row"><p class="message sent">${message}</p><p class="btn-reply">Reply</p></div>`
+    : `<div class="message-row received"><p class="message">${message}</p><p class="btn-reply">Reply</p></div>`;
 
 messages.forEach((message) => {
   beforeEnd(
